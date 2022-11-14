78f06eeac30040f02ca29d58c00af5387b3c65b4
Frankie Sun <UB10682@DOW.COM>
2022-11-14 13:16:15 +0800

    'INIT'

---
+++ Cat.MauiDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cat.MauiDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Cat.MauiDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cat.MauiDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
