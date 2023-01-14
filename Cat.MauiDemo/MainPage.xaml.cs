88f02daa50250e189302979012f8f717a36a8c6c
猫猫球 <45891684+FanSun01@users.noreply.github.com>
2023-01-15 02:02:58 +0800

    update

--- Cat.MauiDemo/MainPage.xaml.cs
+++ Cat.MauiDemo/MainPage.xaml.cs
@@ -9,16 +9,10 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void ResultButton_OnClicked(object sender, EventArgs e)
+    {
+        ResultLabel.Text = "Hello World!";
+    }
 }
 
